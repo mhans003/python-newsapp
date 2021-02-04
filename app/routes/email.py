31728758dd7a8a13ba7311394c8dd21ac0afd743
mhans003 <michaeledwardhanson@gmail.com>
2021-02-03 16:16:02 -0800

    /email/forgot route now sends a test email to email input by the user in forgot.html.

--- app/routes/email.py
+++ app/routes/email.py
@@ -46,4 +46,22 @@ def forgot():
         print(sys.exc_info()[0])
         return jsonify(message = 'Incorrect Credentials'), 400
 
+    # Configure flask mail.
+    current_app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    current_app.config['MAIL_PORT'] = 465
+    current_app.config['MAIL_USE_SSL'] = True
+    current_app.config['MAIL_USERNAME'] = getenv('EMAIL_ADDRESS')
+    current_app.config['MAIL_PASSWORD'] = getenv('EMAIL_PASSWORD')
+
+    # Use the current app instance to create instance of Mail object.
+    mail= Mail(current_app)
+
+    # Configure email message. 
+    msg = Message()
+    msg.subject = "Password Reset Link - Python News App"
+    msg.recipients = [data['email']]
+    msg.sender = getenv('EMAIL_ADDRESS')
+    msg.body = 'You have requested to reset your password.'
+    mail.send(msg)
+
     return jsonify(email = user.email)
